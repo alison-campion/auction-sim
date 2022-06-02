d84846c848cf3005595f8d929b118d0c88006539
saptarshi dartmouth machine <saptarshi3.14@gmail.com>
2022-06-01 23:04:07 -0400

    Added the normal distribution to the distribution of values. This takes considerably longer per iteration (didn't run the 100k iterations; took too long). This needs to be //-ised.

--- revenueequiv_test.py
+++ revenueequiv_test.py
@@ -12,18 +12,26 @@ sns.set(rc = {'figure.figsize':(5,5)})
 def cdf_uniform(x,delta,n):
     return ((x/delta)**(n-1))
 
+## (Truncated) Normal Distribution
+def cdf_normal(x, mean, sd, n):
+    return(((norm(loc=mean, scale = sd).cdf(x))/(norm(loc=mean, scale = sd).cdf(0)))**(n-1))
+
 # Defining auction preliminaries
 # number of players in the auction
 n = 20
 
 # Player valuations
-## Assume uniformly distributed values in [0,100]
+## Uniform distribution in [0,100]
 start = 0
 width = 100
 
+## Normal distribution ~ (50,400), truncated below at 0
+mean = 50
+sd = 20
+
 # Monte-Carlo etc
 # Number of simulations
-n_sim = 100000
+n_sim = 10
 
 # Seller revenue arrays
 FP_revenue = []
@@ -31,14 +39,16 @@ SP_revenue = []
 
 for sim in range(1,n_sim):
 
-    data = uniform.rvs(size=n, loc = start, scale=width)
+#    data = uniform.rvs(size=n, loc = start, scale=width)
+    data = norm.rvs(size=n, loc = mean, scale=sd)
     
     # Player optimal bids 
     
     ## First Price Auction
     FP_optimal_bid = []
     for v in data:
-        FP_bid = v - ((integrate.quad(cdf_uniform, 0, v, args = ((width-start),n))[0])/cdf_uniform(v, (width-start),n))
+#        FP_bid = v - ((integrate.quad(cdf_uniform, 0, v, args = ((width-start),n))[0])/cdf_uniform(v, (width-start),n))
+        FP_bid = v - ((integrate.quad(cdf_normal, 0, v, args = (mean, sd, n))[0])/cdf_normal(v, mean, sd, n))
         FP_optimal_bid.append(FP_bid)
     
     ## Second Price Auction
